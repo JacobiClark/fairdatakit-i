458a6b1146eba2a766b75df8e504c57e0d11fc7c
Jacob Clark <jacobcs@zoho.com>
2023-06-17 15:13:45 -0700

    wip: remove mui tree

--- src/components/DatasetForm.tsx
+++ src/components/DatasetForm.tsx
@@ -6,88 +6,22 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
-interface FolderStructure {
-  name: string;
-  path: string;
-  children: FolderStructure[];
-}
-
 function DatasetForm() {
-  const [folderStructure, setFolderStructure] =
-    useState<FolderStructure | null>(null);
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-
-  const handleSendMessage = () => {
-    window.electron.ipcRenderer.sendMessage('open-folder-dialog', []);
-  };
-
-  useEffect(() => {
-    const handleDatasetStructureMessage = (args: FolderStructure) => {
-      setFolderStructure(args);
-    };
-
-    window.electron.ipcRenderer.on(
-      'open-folder-dialog',
-      handleDatasetStructureMessage
-    );
-  }, []);
-
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleContextMenu = (event: React.MouseEvent<HTMLElement>) => {
-    event.preventDefault();
-    setAnchorEl(event.currentTarget);
-  };
-
-  const renderTree = (nodes: FolderStructure) => (
-    <TreeItem
-      key={nodes.path}
-      nodeId={nodes.path}
-      label={nodes.relativePath}
-      onContextMenu={handleContextMenu}
-    >
-      {nodes.children.map((node) => renderTree(node))}
-    </TreeItem>
-  );
-
   return (
     <>
-      {folderStructure && (
-        <TreeView
-          aria-label="rich object"
-          defaultCollapseIcon={<ExpandMoreIcon />}
-          defaultExpanded={['root']}
-          defaultExpandIcon={<ChevronRightIcon />}
-          multiSelect
-          sx={{
-            height: 500,
-            flexGrow: 1,
-            maxWidth: '100%',
-            overflowY: 'auto',
-          }}
-        >
-          {renderTree(folderStructure)}
-        </TreeView>
-      )}
+      <div style={{ height: 400 }}>
+        <FullFileBrowser
+          files={files}
+          folderChain={folderChain}
+          fileActions={fileActions}
+          onFileAction={handleFileAction}
+          thumbnailGenerator={thumbnailGenerator}
+          {...props}
+        />
+      </div>
       <button type="button" onClick={handleSendMessage}>
         Send IPC Message
       </button>
-      <Menu
-        id="context-menu"
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>Option 1</MenuItem>
-        <MenuItem onClick={handleClose}>Option 2</MenuItem>
-        <MenuItem onClick={handleClose}>Option 3</MenuItem>
-      </Menu>
     </>
   );
 }
